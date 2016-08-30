a5fd4c4b42b1494584b47568cdb93ec99fa9673c
shijian.yang <shijian.yangsd@gmail.com>
2016-08-30 11:24:25 +0900

    web and mobile login merge

--- wechat_user_controller.js
+++ wechat_user_controller.js
@@ -1,9 +1,9 @@
-// 区分用户登录类型
+// 用户整合
 Accounts.onCreateUser(function (options, user) {
     if (options.profile) {
         user.profile = options.profile;
     }
-    
+
     if(user.services.hasOwnProperty("wechat")){
       user.services.webwechat = user.services.wechat;
       return user;
